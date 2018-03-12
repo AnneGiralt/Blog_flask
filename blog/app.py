83dae0ddcef7167d06855fa98e03dd34119ab850
Anne <giralt.anne@gmail.com>
2018-03-12 18:40:13 +0100

    create article just backend

--- blog/app.py
+++ blog/app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from serializer import *
 
-
-
-#from mocks import Post
-
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from create_post import create_post
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 
 class Post(db.Model):
@@ -22,6 +21,14 @@ class Post(db.Model):
     title = db.Column(db.String(255))
     content = db.Column(db.Text)
     note = db.Column(db.Float)
+
+class PostForm(Form):
+    author = StringField('Author') #,[validators.Length(min=1, max=25)])
+    title = StringField('Title') #,[validators.Length(min=1, max=25)])
+    content = StringField('Coment') #,[validators.Length(min=1, max=250)])
+    adress = StringField('Adress') #,[validators.Length(min=1, max=25)])
+    note = StringField('Note') #,[validators.NumberRange(min=0, max=5, message = 'Chose a mark from 0 to 5')])
+
 
 
 
@@ -58,9 +65,16 @@ def get_posts():
     posts =  Post.query.all()
     P = PostSchema(many=True)
     var = P.dump(posts).data
-
     return jsonify(var)
 
+
+@app.route('/addadress', methods=['GET', 'POST'])
+def add_adress():
+    form = PostForm(request.form)
+    if request.method == 'POST' and form.validate():
+        create_post(title = form.title.data, author = form.author.data, content = form.content.data, adress = form.adress.data, note = form.note.data)
+        return redirect(url_for('post_index'))
+    return render_template('posts/newtable.html', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- blog/create_post.py
+++ blog/create_post.py
@@ -1,6 +1,19 @@
-from app import db, Post
+#from app import Post
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at= db.Column(db.DateTime)
+    author = db.Column(db.String(255))
+    adress = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    note = db.Column(db.Float)
 
 def create_post(title,author, content, adress, note):
 	p = Post(title = title, author = author, adress = adress, content = content, created_at = datetime.now(), note =note)
